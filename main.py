a434e44628be7ebde4fde75eebe0fceb13396dae
Emmanuel <emmanuelchan809@gmail.com>
2024-05-09 10:03:02 -0500

    New version

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
 from sympy import *
 import re
 
-print("Ax'+By'+Cx+Dy=Ee^(at+b)")
-print("A1x'+B1y'+C1x+D1y=E1e^(a1t+b1)")
+text_1 = "Ax'+By'+Cx+Dy=Ee^(at+b)"
+text_2 = "A1x'+B1y'+C1x+D1y=E1e^(a1t+b1)"
+print(text_1)
+print(text_2)
 
-D, y, x, t, c1, c2, c3, c4 = symbols('D y x t c1 c2 c3 c4')
+D, y, x, t, c1, c2, c3, c4, X = symbols('D y x t c1 c2 c3 c4 X')
 f = Function('x')(t)
 
+'''
+A = 1
+B=0
+C=0
+d=-4
+F=1
+a=0
+b=0
+
+A1 = 0
+B1=1
+C1=1
+d1=0
+F1=2
+a1=0
+b1=0
+'''
 A = 1
 B = 0
-C = -3
-d = 1
-F = -1
+C = 0
+d = -4
+F = 1
 a = 0
 b = 0
 
 A1 = 0
 B1 = 1
-C1 = -1
-d1 = -1
-F1 = 4
-a1 = 1
+C1 = 1
+d1 = 0
+F1 = 2
+a1 = 0
 b1 = 0
 '''
 print('A')
@@ -68,11 +87,8 @@ def wronskiano(y1, y2, fx):
     return result
 
 det = simplify(Matrix([[A*D+C,B*D+d],[A1*D+C1,B1*D+d1]]).det())
-print(Matrix([[A*D+C,B*D+d],[A1*D+C1,B1*D+d1]]))
-print(det)
 
 expressions = solve(det)
-print(expressions)
 exp1 = 0
 exp2 = 0
 if expressions != []:
@@ -104,11 +120,6 @@ if F == 0 and F1 == 0:
     C2 = solve(simplify(ec1.subs(c1,0).subs(c3,0)),c2)[0]
     C3 = solve(simplify(ec1.subs(c2,0).subs(c4,0)),c3)[0]
     C4 = solve(simplify(ec1.subs(c1,0).subs(c3,0)),c4)[0]
-
-    print(C1)
-    print(C2)
-    print(C3)
-    print(C4)
 
     new_x = x.subs(c1, C1).subs(c2, C2)
 
@@ -146,8 +157,75 @@ else:
     xp = wronskiano(y1, y2, detx)
     x = expand(simplify(x+xp))
     print('x=',x)
-
     yp = wronskiano(y1, y2, dety)
-    y = expand(simplify(y+yp))
+    wronskiano_y = expand(simplify(y+yp))
+    ec1 = Eq(A*Derivative(x, t) + B*Derivative(y, t) + C*x + d*y,F*E**(a*t+b))
+    solve_x = solve(ec1, X)
+    solve_dx = solve(ec1, Derivative(X, t))
+    if solve_x != []:
+        y = expand(solve_x[0])
+    elif solve_dx != []:
+        solve_dx = integrate(solve_dx[0], t)+c2
+        y = expand(solve_dx)
+
+    if 'X' in str(y) or 'Derivative(X, t)' in str(y) or (solve_x == [] and solve_dx == []):
+        ec2 = Eq(A1*Derivative(x, t) + B1*Derivative(X, t) + C1*x + d1*X,F1*E**(a1*t+b1))
+        solve_x_1 = solve(ec2, X)
+        solve_dx_1 = solve(ec2, Derivative(X, t))
+        if solve_x_1 != []:
+            y = expand(solve_x_1[0])
+        elif solve_dx_1 != []:
+            solve_dx_1 = integrate(solve_dx_1[0], t)+c2
+            y = expand(solve_dx_1)
+
+    if 'X' in str(y) or 'Derivative(X, t)' in str(y):
+        y = wronskiano_y
     print('y=',y)
-    #wronskiano(y1,y2,dety)
+
+    print('Sustituir en la parte homogenea de la primera ecuacion:')
+    result1 = simplify(A*Derivative(x, t) + B*Derivative(y, t) + C*x + d*y)
+    print(simplify(result1))
+    if "c1" in str(simplify(result1)):
+        c_value = solve(Eq(result1,F*E**(a*t+b)), c1)
+        if c_value!= []:
+            print('Resultado actualizado:')
+            x = expand(x.subs(c1,c_value[0]))
+            y = expand(y.subs(c1,c_value[0]))
+            print('x=',x)
+            print('y=',y)
+        result1 = result1.subs(c1, c_value[0])
+    if 'c2' in str(simplify(result1)):
+        c_value = solve(Eq(result1,F*E**(a*t+b)), c2)
+        if c_value != []:
+            print('Resultado actualizado:')
+            x = expand(x.subs(c2,c_value[0]))
+            y = expand(y.subs(c2,c_value[0]))
+            print('x=',x)
+            print('y=',y)
+        result1 = result1.subs(c2, c_value[0])
+    print('Sustituir en la parte homogenea de la segunda ecuacion:')
+    result2 = simplify(A1*Derivative(x, t) + B1*Derivative(y, t) + C1*x + d1*y)
+    print(simplify(result2))
+    if 'c1' in str(simplify(result2)):
+        c_value = solve(Eq(result2,F1*E**(a1*t+b1)), c1)
+        if c_value != []:
+            print('Resultado actualizado:')
+            x = expand(x.subs(c1,c_value[0]))
+            y = expand(y.subs(c1,c_value[0]))
+            print('x=',x)
+            print('y=',y)
+        result2 = result2.subs(c2, c_value[0])
+    if 'c2' in str(simplify(result2)):
+        c_value = solve(Eq(result2,F1*E**(a1*t+b1)), c2)
+        if c_value != []:
+            print('Resultado actualizado:')
+            x = expand(x.subs(c2,c_value[0]))
+            y = expand(y.subs(c2,c_value[0]))
+            print('x=',x)
+            print('y=',y)
+        result2 = result2.subs(c2, c_value[0])
+
+    if expand(result1) == expand(F*E**(a*t+b)) and expand(result2) == expand(F1*E**(a1*t+b1)):
+        print('El resultado es exacto')
+        print('x =',x)
+        print('y =',y)
