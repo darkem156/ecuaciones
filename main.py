82acd40fe2de235f9a19530467658a92783fbb37
Emmanuel <emmanuelchan809@gmail.com>
2024-05-13 10:41:02 -0500

    Fixed errors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sympy import *
 import re
+import sys
 
 text_1 = "Ax'+By'+Cx+Dy=Ee^(at+b)"
 text_2 = "A1x'+B1y'+C1x+D1y=E1e^(a1t+b1)"
@@ -9,36 +10,19 @@ print(text_2)
 D, y, x, t, c1, c2, c3, c4, X = symbols('D y x t c1 c2 c3 c4 X')
 f = Function('x')(t)
 
-'''
-A = 1
-B=0
-C=0
-d=-4
-F=1
-a=0
-b=0
-
-A1 = 0
-B1=1
-C1=1
-d1=0
-F1=2
-a1=0
-b1=0
-'''
 A = 1
 B = 0
-C = 0
-d = -4
+C = 1
+d = -1
 F = 1
 a = 0
 b = 0
 
 A1 = 0
 B1 = 1
-C1 = 1
-d1 = 0
-F1 = 2
+C1 = 2
+d1 = -1
+F1 = cot(t)
 a1 = 0
 b1 = 0
 '''
@@ -92,7 +76,6 @@ expressions = solve(det)
 exp1 = 0
 exp2 = 0
 if expressions != []:
-    print(expressions)
     if "I" not in str(expressions):
         expressions.sort(reverse=False)
     if len(expressions) == 1:
@@ -119,7 +102,7 @@ y = c1*y1+c2*y2
 def replace_D(text):
     text = str(text)
     text = text.replace("D", "1D")
-    elements = re.split(r"([+\-=\s])", text)
+    elements = re.split(r"(?<![a-zA-Z]\()([+\-\s=])", text)
     for i, elem in enumerate(elements):
         if "1D" in elem:
             match = re.search(r"1D\*\*(\d+)", elem)
@@ -137,26 +120,17 @@ if True:
     detx = simplify(replace_D(detx))
     dety = simplify(replace_D(dety))
 
-    print(detx)
-    print(dety)
-
     if 'D' not in str(det):
         x = detx/det
         y = dety/det
         print('x=',x)
         print('y=',y)
         exit()
-    #print(x)
-    #print(y)
-    #print(detx)
-    #print(dety)
-    #ec = solve(Eq(det, detx), D)
+    print(Eq(det,detx))
     xp = wronskiano(y1, y2, detx)
     x = expand(simplify(x+xp))
-    print(x)
     yp = wronskiano(y1, y2, dety)
     wronskiano_y = expand(simplify(y+yp))
-    print(wronskiano_y)
     ec1 = Eq(A*Derivative(x, t) + B*Derivative(X, t) + C*x + d*X,F*E**(a*t+b))
     solve_x = solve(ec1, X)
     solve_dx = solve(ec1, Derivative(X, t))
@@ -182,62 +156,64 @@ if True:
         solve_ec1_x = solve(ec1, X)
         if solve_ec1_x != []:
             if 'Derivative(X, t)' not in str(solve_ec1_x):
-                print('solving x on ec1')
-        ec2 = Eq(A1*Derivative(X, t) + B1*Derivative(y, t) + C1*X + d1*y,F1*E**(a1*t+b1))
-        solve_ec2_x = solve(ec2, X)
-        if solve_ec2_x != []:
-            if 'Derivative(X, t)' not in str(solve_ec2_x):
-                print('solving x on ec2')
-                x = solve_ec2_x[0]
+                #print('solving x on ec1')
+                x = solve_ec1_x[0]
+        else:
+            ec2 = Eq(A1*Derivative(X, t) + B1*Derivative(y, t) + C1*X + d1*y,F1*E**(a1*t+b1))
+            solve_ec2_x = solve(ec2, X)
+            if solve_ec2_x != []:
+                if 'Derivative(X, t)' not in str(solve_ec2_x):
+                    #print('solving x on ec2')
+                    x = solve_ec2_x[0]
     print('x=',x)
     print('y=',y)
-
-    '''
-    print('Sustituir en la parte homogenea de la primera ecuacion:')
-    result1 = simplify(A*Derivative(x, t) + B*Derivative(y, t) + C*x + d*y)
-    print(simplify(result1))
-    if "c1" in str(simplify(result1)):
-        c_value = solve(Eq(result1,F*E**(a*t+b)), c1)
-        if c_value!= []:
-            print('Resultado actualizado:')
-            x = expand(x.subs(c1,c_value[0]))
-            y = expand(y.subs(c1,c_value[0]))
-            print('x=',x)
-            print('y=',y)
-        result1 = result1.subs(c1, c_value[0])
-    if 'c2' in str(simplify(result1)):
-        c_value = solve(Eq(result1,F*E**(a*t+b)), c2)
-        if c_value != []:
-            print('Resultado actualizado:')
-            x = expand(x.subs(c2,c_value[0]))
-            y = expand(y.subs(c2,c_value[0]))
-            print('x=',x)
-            print('y=',y)
-        result1 = result1.subs(c2, c_value[0])
-    print('Sustituir en la parte homogenea de la segunda ecuacion:')
-    result2 = simplify(A1*Derivative(x, t) + B1*Derivative(y, t) + C1*x + d1*y)
-    print(simplify(result2))
-    if 'c1' in str(simplify(result2)):
-        c_value = solve(Eq(result2,F1*E**(a1*t+b1)), c1)
-        if c_value != []:
-            print('Resultado actualizado:')
-            x = expand(x.subs(c1,c_value[0]))
-            y = expand(y.subs(c1,c_value[0]))
-            print('x=',x)
-            print('y=',y)
-        result2 = result2.subs(c2, c_value[0])
-    if 'c2' in str(simplify(result2)):
-        c_value = solve(Eq(result2,F1*E**(a1*t+b1)), c2)
-        if c_value != []:
-            print('Resultado actualizado:')
-            x = expand(x.subs(c2,c_value[0]))
-            y = expand(y.subs(c2,c_value[0]))
-            print('x=',x)
-            print('y=',y)
-        result2 = result2.subs(c2, c_value[0])
-
-    '''
     result1 = simplify(A*Derivative(x, t) + B*Derivative(y, t) + C*x + d*y)
     result2 = simplify(A1*Derivative(x, t) + B1*Derivative(y, t) + C1*x + d1*y)
     if expand(result1) == expand(F*E**(a*t+b)) and expand(result2) == expand(F1*E**(a1*t+b1)):
         print('El resultado es exacto')
+        exit()
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "sustituir":
+            print('Sustituir en la parte homogenea de la primera ecuacion:')
+            result1 = simplify(A*Derivative(x, t) + B*Derivative(y, t) + C*x + d*y)
+            print(simplify(result1))
+            if "c1" in str(simplify(result1)):
+                c_value = solve(Eq(result1,F*E**(a*t+b)), c1)
+                if c_value!= []:
+                    print('Resultado actualizado:')
+                    new_x = expand(x.subs(c1,c_value[0]))
+                    new_y = expand(y.subs(c1,c_value[0]))
+                    print('x=',new_x)
+                    print('y=',new_y)
+                result1 = result1.subs(c1, c_value[0])
+            if 'c2' in str(simplify(result1)):
+                c_value = solve(Eq(result1,F*E**(a*t+b)), c2)
+                if c_value != []:
+                    print('Resultado actualizado:')
+                    new_x = expand(x.subs(c2,c_value[0]))
+                    new_y = expand(y.subs(c2,c_value[0]))
+                    print('x=',new_x)
+                    print('y=',new_y)
+                result1 = result1.subs(c2, c_value[0])
+            print('Sustituir en la parte homogenea de la segunda ecuacion:')
+            result2 = simplify(A1*Derivative(x, t) + B1*Derivative(y, t) + C1*x + d1*y)
+            if 'c1' in str(simplify(result2)):
+                c_value = solve(Eq(result2,F1*E**(a1*t+b1)), c1)
+                print(c_value[0])
+                if c_value != []:
+                    print('Resultado actualizado:')
+                    x = expand(x.subs(c1,c_value[0]))
+                    y = expand(y.subs(c1,c_value[0]))
+                    print('x=',x)
+                    print('y=',y)
+                result2 = result2.subs(c2, c_value[0])
+            if 'c2' in str(simplify(result2)):
+                c_value = solve(Eq(result2,F1*E**(a1*t+b1)), c2)
+                if c_value != []:
+                    print('Resultado actualizado:')
+                    x = expand(x.subs(c2,c_value[0]))
+                    y = expand(y.subs(c2,c_value[0]))
+                    print('x=',x)
+                    print('y=',y)
+                result2 = result2.subs(c2, c_value[0])
